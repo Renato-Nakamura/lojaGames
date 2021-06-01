3260c29d1803d0bec21a42a04f5399d1c0566f08
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-06-01 09:38:01 -0300

    spring

---
+++ eCommerceAbreu/src/test/java/com/lojinhaDoAbreu/eCommerceAbreu/ECommerceAbreuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojinhaDoAbreu.eCommerceAbreu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAbreuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
